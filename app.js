4e6363249937ec620c32e261666a806737862ce2
jayeen29 <jayeen6783@gmail.com>
2021-08-18 17:35:52 +0600

    function added for case input

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('I am in a separate file')
+//functions
+function updateCaseNumber(inputId, isAdd) {
+    const caseInput = document.getElementById(inputId);
+    const caseText = caseInput.value;
+    const caseNumber = parseInt(caseText);
+    if (isAdd == true) {
+        caseInput.value = caseNumber + 1;
+    }
+    else {
+        if (caseNumber > 0) {
+            caseInput.value = caseNumber - 1;
+        }
+    }
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateCaseNumber('case-input', true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber('case-input', false);
+})
